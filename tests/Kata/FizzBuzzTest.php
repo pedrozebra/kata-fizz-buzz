30ea06d2fbc871ca241405e3d599bede1ce2000a
pedrozebra <pdellanotte@gmail.com>
2023-04-13 22:29:23 +0200

    feat: added testProgramWriteRangeNumbers

--- tests/Kata/FizzBuzzTest.php
+++ tests/Kata/FizzBuzzTest.php
@@ -23,4 +23,12 @@ class FizzBuzzTest extends TestCase
     {
         $this->assertEquals(true, $this->fizzBuzz->handle());
     }
+
+    public function testProgramWriteRangeNumbers() {
+        $result = $this->fizzBuzz->writeRangeNumbers(1,100);
+        $this->assertIsArray($result);
+        $this->assertCount(100, $result);
+        $this->assertEquals(1, $result[0]);
+        $this->assertEquals(100, $result[99]);
+    }
 }
