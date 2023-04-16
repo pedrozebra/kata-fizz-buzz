db0932e7347f3dde98f6bfc342d1f6bd05b336b8
pedrozebra <pdellanotte@gmail.com>
2023-04-16 12:36:10 +0200

    feat: added methods on generate fizz buzz  green tests

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -1,18 +1,24 @@
-<?php declare(strict_types=1);
+<?php
 
 namespace Kata;
 
 class FizzBuzz
 {
+    private $listRangeNumbers = [];
+
+    public function __construct()
+    {
+        $this->listRangeNumbers = $this->writeRangeNumbers(1, 100);
+    }
     public function handle(): bool
     {
         return true;
     }
-    
+
     public function writeRangeNumbers(int $start, int $end): array
     {
         $result = [];
-        for($i = $start; $i <= $end; $i++) {
+        for ($i = $start; $i <= $end; $i++) {
             $result[] = $i;
         }
         return $result;
@@ -26,5 +32,25 @@ class FizzBuzz
     public function isMultipleOfFive(int $num): bool
     {
         return $num % 5 === 0;
+    }
+
+    public function isMultipleOfThreeAndFive(int $num): bool
+    {
+        return $this->isMultipleOfThree($num) && $this->isMultipleOfFive($num);
+    }
+
+    public function generateFizzBuzz(): array
+    {
+        $result = [];
+
+        $result = array_map(function ($item) {
+            if ($this->isMultipleOfThreeAndFive($item)) return 'FizzBuzz';
+            if ($this->isMultipleOfThree($item)) return 'Fizz';
+            if ($this->isMultipleOfFive($item)) return 'Buzz';
+
+            return $item;
+        }, $this->listRangeNumbers);
+
+        return $result;
     }
 }
